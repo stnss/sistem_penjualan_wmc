0c5d5ba28d6561c63982dc67952338593f79009a
detsgan <detsgan@gmail.com>
2021-06-24 22:58:14 +0700

    feat: adding order

--- resources/views/pages/admin/orders/index.blade.php
+++ resources/views/pages/admin/orders/index.blade.php
@@ -4,7 +4,7 @@
 <div class="content">
     <div class="page-inner">
         <div class="page-header">
-            <h4 class="page-title">Barang</h4>
+            <h4 class="page-title">Order</h4>
             <ul class="breadcrumbs">
                 <li class="nav-home">
                     <a href="{{ route('dashboard') }}">
@@ -15,7 +15,7 @@
                     <i class="flaticon-right-arrow"></i>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('admin.barangs.index') }}">{{ __('menu.list', ['type' => 'Barang']), }}</a>
+                    <a href="{{ route('admin.orders.index') }}">{{ __('menu.list', ['type' => 'Order']), }}</a>
                 </li>
             </ul>
         </div>
@@ -30,7 +30,7 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="card-title">{{ __('menu.list', ['type' => 'Barang']), }}</h4>
+                        <h4 class="card-title">{{ __('menu.list', ['type' => 'Order']), }}</h4>
 
                     </div>
                     <div class="card-body">
@@ -39,55 +39,54 @@
                                 <thead>
                                     <tr>
                                         <th>No.</th>
+                                        <th>Kode Order</th>
                                         <th>Nama Barang</th>
-                                        <th>Stok</th>
+                                        <th>Jumlah Order</th>
+                                        <th>Harga</th>
                                         <th>SKU</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @forelse ($barangs as $barang)
+                                    @forelse ($orders as $order)
                                         <tr>
                                             <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $barang->external_id }}</td>
-                                            <td>{{ $barang->name }}</td>
+                                            <td>{{ $order->external_id }}</td>
+                                            <td>{{ $order->name }}</td>
                                             <td>
                                                 <div class="form-button-action">
-                                                    @can('update-barang')
-                                                        <a href="{{ route('barangs.edit', $barang) }}"
-                                                            data-toggle="tooltip" title=""
-                                                            class="btn btn-link btn-primary btn-lg"
-                                                            data-original-title="Edit Task">
-                                                            <i class="fa fa-edit"></i>
-                                                        </a>
-                                                    @endcan
-
-                                                    @can('delete-barang')
-                                                        <form action="{{ route('barangs.destroy', $barang) }}"
-                                                            method="post">
-                                                            @method('DELETE')
-                                                            @csrf
-                                                            <button type="submit" data-toggle="tooltip" title=""
-                                                                class="btn btn-link btn-danger"
-                                                                data-original-title="Remove">
-                                                                <i class="fa fa-times"></i>
-                                                            </button>
-                                                        </form>
-                                                    @endcan
+                                                    <a href="{{ route('orders.edit', $order) }}"
+                                                        data-toggle="tooltip" title=""
+                                                        class="btn btn-link btn-primary btn-lg"
+                                                        data-original-title="Edit Task">
+                                                        <i class="fa fa-edit"></i>
+                                                    </a>
+                                                    <form action="{{ route('orders.destroy', $order) }}"
+                                                        method="post">
+                                                        @method('DELETE')
+                                                        @csrf
+                                                        <button type="submit" data-toggle="tooltip" title=""
+                                                            class="btn btn-link btn-danger"
+                                                            data-original-title="Remove">
+                                                            <i class="fa fa-times"></i>
+                                                        </button>
+                                                    </form>
                                                 </div>
                                             </td>
                                         </tr>
                                     @empty
                                         <tr>
-                                            <td colspan="4">{{ __('message.not_found', ['type' => 'Barang']) }}</td>
+                                            <td colspan="4">{{ __('message.not_found', ['type' => 'Order']) }}</td>
                                         </tr>
                                     @endforelse
                                 </tbody>
                                 <tfoot>
                                     <tr>
                                         <th>No.</th>
+                                        <th>Kode Order</th>
                                         <th>Nama Barang</th>
-                                        <th>Stok</th>
+                                        <th>Jumlah Order</th>
+                                        <th>Harga</th>
                                         <th>SKU</th>
                                         <th>Action</th>
                                     </tr>
